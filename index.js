563ab2b8dd2b0d1da183a4c35584634d4099072c
Edward Timmer <edtimmer@gmail.com>
2019-05-05 21:57:57 -0400

    building http server

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const Person = require('./person');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const person = new Person("john", 30);
-person.greeting();
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+// const Person = require('./person');
+
+// const person = new Person("john", 30);
+// person.greeting();
+
+// const Logger = require('./logger');
+
+// const logger = new Logger();
+
+// logger.on('message', data => console.log('Caller Listener', data));
+
+// logger.log('Hello World');
+// logger.log('Live Long And Prosper!');
+// logger.log('Hi!');
+
+
